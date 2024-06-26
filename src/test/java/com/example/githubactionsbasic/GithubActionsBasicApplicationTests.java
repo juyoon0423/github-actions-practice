54e452f3c3da356fbf967111338a93f14c344b17
juyoon <juyoon0423@daum.net>
2024-06-26 14:17:40 +0900

    first commit

---
+++ src/test/java/com/example/githubactionsbasic/GithubActionsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
